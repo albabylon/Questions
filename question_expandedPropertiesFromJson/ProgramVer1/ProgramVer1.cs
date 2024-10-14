a84a88acb961c70a5f9475cec1b5d4152cb61137
aveA <aveA@AVE>
2024-10-14 23:56:34 +0300

    ok

---
+++ question_expandedPropertiesFromJson/ProgramVer1/ProgramVer1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question_expandedPropertiesFromJson
+{
+    internal class ProgramVer1
+    {
+        private class PersonData
+        {
+            public string Name { get; set; }
+            public string Surname { get; set; }
+            public int Age { get; set; }
+        }
+
+        private static void GetDataFromJson ()
+        {
+            //метод заполняет PersonData из jsonData.json - по совпаданию имен свойства и параметра в json
+        }
+
+        static void Main(string[] args)
+        {
+            List<PersonData> datas = new List<PersonData> ();
+            GetDataFromJson(); //заполняем список datas по json
+        }
+    }
+}
---
+++ question_expandedPropertiesFromJson/ProgramVer2/ProgramVer2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question_expandedPropertiesFromJson
+{
+    internal class ProgramVer2
+    {
+        private class PersonData
+        {
+            //неопределенное количество полей, должно определяться из jsonProperties.json
+        }
+        private static void GetDataFromJson()
+        {
+            //метод заполняет PersonData из jsonData.json - по совпаданию имен свойства и параметра в json
+        }
+        private static void GetPropertiesFromJson()
+        {
+            //метод который получает из jsonProperties.json настраимовые извне поля для PersonData, которые в дальнейшем может заполнить GetDataFromJson()
+        }
+
+        static void Main(string[] args)
+        {
+            GetPropertiesFromJson(); //получает возможные поля из jsonProperties.json
+            
+            List<PersonData> datas = new List<PersonData>();           
+            GetDataFromJson(); //заполняем список datas по json
+        }
+    }
+}
