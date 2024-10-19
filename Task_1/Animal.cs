bd4f4448ba793d4f05a246e10e4591851484a50f
aveA <aveA@AVE>
2024-10-19 22:24:36 +0300

    Task 1

---
+++ Task_1/Animal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+        public int Popularity { get; set; }
+        public Zoo Zoo { get; internal set; }
+        public Animal(string name, int popularity) 
+        {
+            Name = name;
+            Popularity = popularity;
+        }
+    }
+}
---
+++ Task_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Zoo zoo = new Zoo();
+            zoo.Animals.Add(new Animal("Kangaroo", 10));
+            zoo.Animals.Add(new Animal("Mr Sea Lion", 20));
+
+            foreach (var a in zoo.Animals)
+            {
+                Console.WriteLine(a.Name);
+            }
+            Console.ReadLine();
+        }
+    }
+}
